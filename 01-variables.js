c4c425ddebb241608f4bbed8954c263b199e04f7
Esteban Lopez <estebanlopez5015@gmail.com>
2025-08-19 16:03:51 -0500

    declaracion de variables y tipo de dato string

---
+++ 01-variables.js
@@ -0,0 +1,62 @@
+// alert("Hello world")
+// ==============================
+// CONVENCION NOMBRE PARA CODIFICAR
+// ===============================
+
+// UpperCamelCase  => En JS se usa para nombrar las clases
+// lowerCamelCase  => En JS se usa para nombrar las variables
+// CREAMING_SNAKE_CASE  => En JS se usa para nombrar las constantes.
+
+
+// snake_case      => En JS generalmente no se usa, esta convencion es muy usada en el lenguaje python.
+
+// kebab-case      => Este tipo de sintaxis se utiliza para las urls, nombres de paquetes en Composer
+//                    y también se utiliza como convención de nombres en Lisp. Por ello también es 
+//                    conocido por spinal-case o lisp-case
+
+// Train-case      => Es una variedad del kebab-case, pero cada palabra con la primera letra en mayúsculas.
+//                    Por ejemplo, Mi-Blog-De-Desarrollo.
+
+// variables
+// Ya no se declara  una variable con var
+// En JS no es obligatorio terminar una sentencia con ;
+// En JS no es obligatorio indicar el tipo de  variable
+
+var product = "sensor-1";
+
+// No se puede iniciar una variable con - al medio
+// var -sensor = "sensor-2";
+
+// No se puede iniciar una variable con numero
+// var  1sensor = "sensor3";
+
+// Si es permitido iniciar una variable con guion al piso
+var _sensor = "sensor-4";
+
+// Crear una variable con las tres formas
+
+var sensor = "Sensor de lluvia";
+var sensor = "Sensor de temperatura";
+var sensor = "Sensor de presion";
+
+console.log(_sensor);
+
+let userName = "Edwar";
+// let userName = "Patricia";
+console.log(userName)
+
+// Ejemplo const
+// No puedo cambiarle el valor a una variable constante
+const maxSize = 1024;
+// maxSize = 123456;
+
+// Otro ejemplo con var
+
+console.log(userName2);
+
+// let userName2 = "Jacobo";
+let userName2 = "Jacobo";
+
+
+
+
